2a482962a21a36a0185eb396c47f42fbfc5e8d88
Maaz Irfan <muhammadmaazirfan@outlook.com>
2017-12-26 19:36:18 +0500

    drag object using touch

---
+++ moveObj.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveObj : MonoBehaviour {
+
+    float distance = 10f;
+    public Camera mainCam;
+
+    public float smooth;
+
+    
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+       Vector3 ObjPosition = mainCam.ScreenToWorldPoint(mousePosition);
+       
+
+        transform.position = ObjPosition;
+       // transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+
+    
+
+
+
+}
